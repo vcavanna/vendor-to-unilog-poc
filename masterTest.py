f4e86547eaf9bf83837a3f22ea1ad8b8e72a4670
vincentCav <vin100.cav@gmail.com>
2024-01-26 15:53:57 -0600

    Added sheet classes and functions, began tests

---
+++ masterTest.py
@@ -0,0 +1,76 @@
+import unittest
+import solution.sheetClasses as sheets
+
+m = sheets.Master("problem_set\\test_master_source_dummy.xlsx")
+flangeMountSubset = m.getGroupedCategories().get_group("10000030 FLANGE MOUNT")
+
+
+class TestMaster(unittest.TestCase):
+    def testInit(self):
+        assert (
+            m.masterdf["Part Number"][0] == "10000001"
+        ), "masterdf['Part Number'][0] == {}".format(m.masterdf["Part Number"][0])
+
+    def testGetGroupedCategories(self):
+        self.assertEqual(flangeMountSubset["Rod Spacing"][2], "Loosely Coupled")
+
+    def testGetSeriesPopulationData(self):
+        expectedPopulation = 60
+        actualPopulation = m.getSeriesPopulationData(m.masterdf, "Rod Spacing")
+        self.assertEqual(actualPopulation, expectedPopulation)
+
+    def testGetDescriptorColumns(self):
+        actualDescriptorColumns = [
+            "Actual Ratio",
+            "ERPA Size",
+            "Angular Offset",
+            "TPSA Certificate",
+            "Axial Offset",
+            "Backstop Included",
+            "Bearing Series",
+            "Base to Center Height",
+            "Base to Flange Center Height",
+            "Industrial Component",
+            "Industrial Part Type",
+            "Industrial Part Height",
+            "Industrial Part Length Datum",
+            "Gear Circle",
+            "Gear Diameter",
+            "Gear to Gear (Max)",
+            "Hole Diameter (Maximum)",
+            "Hole Diameter (Minimum)",
+            "Center Distance",
+            "Center to End of Rod Distance",
+            "Depth",
+            "Rod Length Input",
+            "Rod Length Output",
+            "Rod Spacing",
+        ]
+        expectedDescriptorColumns = m.getDescriptorColumns()
+
+        self.assertEqual(set(actualDescriptorColumns), set(expectedDescriptorColumns))
+
+    def testGetTopCategoryDescriptors(self):
+        expectedTopDescriptors = set(
+            [
+                "Rod Spacing",
+                "TPSA Certificate",
+                "Industrial Part Type",
+            ]
+        )
+        actualTopDescriptors = set(m.getTopCategoryDescriptors(flangeMountSubset))
+
+        self.assertEquals(expectedTopDescriptors, actualTopDescriptors)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+# class TestCalculations(unittest.TestCase):
+
+#     def test_sum(self):
+#         calculation = Calculations(8, 2)
+#         self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong.')
+
+# if __name__ == '__main__':
+#     unittest.main()
---
+++ solution/sheetClasses.py
@@ -0,0 +1,115 @@
+import pandas as pd
+
+NOT_MASTER_DESCRIPTORS = set(
+    [
+        "Description",
+        "Part Number",
+        "Categories",
+        "Category Names",
+        "Enabled",
+    ]
+)
+
+MASTER_NULL_SYMBOL = "$"
+MASTER_CATEGORY_COLUMN = "Description"
+
+
+class Master:
+    def __init__(self, masterSheet) -> None:
+        self.masterdf = pd.read_excel(masterSheet)
+
+    def getGroupedCategories(self, column=MASTER_CATEGORY_COLUMN):
+        return self.masterdf.groupby(column)
+
+    def getSeriesPopulationData(self, df, column, char=MASTER_NULL_SYMBOL):
+        # get the % not populated by '$' (with exceptions)
+        percentPopulated = df[column].value_counts(normalize=True)
+        if not ("$" in percentPopulated.index):
+            return 100
+        return (1 - percentPopulated[MASTER_NULL_SYMBOL]) * 100
+
+    def getDescriptorColumns(self):
+        # returns a list of only the descriptor columns
+
+        # get a list of master_source columns
+        allColumns = self.masterdf.columns.values
+        # filter that list to only include descriptors (so don't include the enum NOT_MASTER_DESCRIPTORS, or '(unit)' categories)
+        descriptorColumns = filter(
+            lambda x: not (x in NOT_MASTER_DESCRIPTORS or "(Unit)" in x), allColumns
+        )
+        return list(descriptorColumns)
+
+    # df containing only one category
+    def getTopCategoryDescriptors(self, df):
+        # return a list of the top 5 category descriptors
+        columns = self.getDescriptorColumns()
+        cdData = {"names": [], "popPercent": []}
+        for column in columns:
+            percentPopulated = self.getSeriesPopulationData(df, column)
+            if percentPopulated > 0:
+                cdData["names"].append(column)
+                cdData["popPercent"].append(percentPopulated)
+
+        topFiveDf = pd.DataFrame(cdData).nlargest(5, "popPercent")
+        return topFiveDf["names"].values
+
+    def wrapTopDescriptors(self):
+        # return df of descriptors, with associated UOM if there is one.
+        # columns included: CATEGORY_CODE, CATEGORY_NAME, DESCRIPTOR_NAME, DISP_SEQ (auto generated), FILTER_ENABLED, FILTER_SEQ
+        pass
+
+    def getTopDescriptors():
+        # per each category, getTopCategoryDescriptors
+        # add all the dataframes together
+        # return a dataframe of descriptors
+        pass
+
+
+class CategoryDescriptors:
+    def __init__(self, cdSheet):
+        self.cdDf = pd.read_excel(cdSheet)
+        self.name = "filled_" + cdSheet
+
+    def addConstantColumns(self):
+        # adds the unchanging columns to the dataframe, then returns it
+        # input is a dataframe
+        self.cdDf["GROUP_NAME"] = pd.series("CNSTNT")
+        self.cdDf["DATA_TYPE"] = pd.series("T")
+        self.cdDf["DESCRIPTOR_TYPE"] = pd.series("Value")
+        self.cdDf["VALUE_DELIMITER"] = pd.series("")
+        self.cdDf["IS_DIFFERENTIATOR_DESCRIPTOR"] = pd.series("N")
+        self.cdDf["ENTITY_TYPE"] = pd.series("I")
+        self.cdDf["PRINT"] = pd.series("Y")
+        self.cdDf["STATUS"] = pd.series("Y")
+
+    def addDescriptors(self, addedDescriptors):
+        # intakes a DF as from getTopDescriptors, modifies to the shape of CategoryDescriptors sheet, then adds to the sheet
+        pass
+
+    def exportToSheet(self):
+        pass
+
+
+class Destination:
+    def __init__(self, destinationSheet) -> None:
+        self.destinationDf = pd.read_excel(destinationSheet)
+        self.name = "filled_" + destinationSheet
+
+    def getOpenDescriptor(self) -> int:
+        pass
+
+    def addDescriptorName(self, name, value, uom="") -> None:
+        pass
+
+    def addDescriptors(self):
+        # join descriptors from CategoryDescriptors to Destination on "category"
+
+        pass
+
+    def addValues(self):
+        # for each row: get the matching master product entry
+        # for each descriptor in row: add the corresponding value in the product entry
+        pass
+
+    def exportToSheet(self):
+        pass
